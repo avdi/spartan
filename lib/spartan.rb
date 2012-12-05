a940af666ba0e2c6725d7fab623481241b42a816
Oliver Ponder <oliver.ponder@gmail.com>
2012-12-05 06:05:38 +0100

    Adds rainbows

--- lib/spartan.rb
+++ lib/spartan.rb
@@ -1,3 +1,4 @@
+require 'rainbow'
 module Spartan
 
   def verify_file(file_path, options = {})
@@ -7,7 +8,9 @@ module Spartan
     include Rcodetools
     example_file = file_path
     example_code = File.read(example_file)
+
     xmp_options = options.fetch(:xmp_options){{}}
+    colorize = options[:colorize]
 
     output = ::XMPFilter.run(example_code,xmp_options)
     diff   = IO.popen("diff -u #{example_file} -", "r+")
@@ -16,7 +19,14 @@ module Spartan
     results = diff.read
     diff.close_read
     if $CHILD_STATUS.success?
-      puts "Green!"
+      message = "Green!"
+
+      if colorize
+        message = message.color(:green)
+      end
+
+      puts message
+
       exit 0
     else
       puts results
